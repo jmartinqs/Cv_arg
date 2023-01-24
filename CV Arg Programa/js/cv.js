fce360748ad8b78ad517392bdec99438ac7439e3
jmartinqs <martinqs@live.com.ar>
2023-01-24 19:09:01 -0300

    first

---
+++ CV Arg Programa/js/cv.js
@@ -0,0 +1,11 @@
+"strict mode";
+
+function classToggle() {
+  const navs = document.querySelectorAll(".navbar-items");
+
+  navs.forEach((nav) => nav.classList.toggle("navbar-toggleshow"));
+}
+
+document
+  .querySelector(".navbar-link-toggle")
+  .addEventListener("click", classToggle);
