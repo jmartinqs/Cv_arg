b43243684ea0170bc05ba5c26aa7ed181eff0f22
jmartinqs <martinqs@live.com.ar>
2023-01-26 19:00:35 -0300

    probando botones js

--- CV Arg Programa/js/cv.js
+++ CV Arg Programa/js/cv.js
@@ -9,3 +9,15 @@ function classToggle() {
 document
   .querySelector(".navbar-link-toggle")
   .addEventListener("click", classToggle);
+
+// document.getElementById('botonGm').addEventListener('click') = "location.href'https://gmail.com';"
+
+// element.addEventListener("click", function() {
+//   document.getElementById("botonGm").innerHTML = href'https://gmail.com';
+// });
+
+const botonGmail = document.querySelector('botonGm');
+
+botonGmail.addEventListener('click', function () {
+  'https://gmail.com'
+})
