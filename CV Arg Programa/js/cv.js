46fee4fcae668596885e58246a92d1577b10dd25
jmartinqs <martinqs@live.com.ar>
2023-01-28 12:06:31 -0300

    Finalizando con Js

--- CV Arg Programa/js/cv.js
+++ CV Arg Programa/js/cv.js
@@ -1,5 +1,12 @@
 "strict mode";
+// Curriculum vitae
 
+document.getElementById("Cvhead").addEventListener("click", function () {
+  console.log("Curriculum Vitae proyecto Arg Programa");
+  alert("Bienvenidos a mi proyecto");
+});
+
+/// Elementos toggle web movil
 function classToggle() {
   const navs = document.querySelectorAll(".navbar-items");
 
@@ -10,5 +17,22 @@ document
   .querySelector(".navbar-link-toggle")
   .addEventListener("click", classToggle);
 
-document.getElementById("botonGm").addEventListener("click").href =
-  "https://gmail.com";
+/// Links contacto
+
+document.getElementById("btnGmail").href = "https://gmail.com";
+
+document.getElementById("Email").addEventListener("click", function () {
+  console.log("Aqui encontrariamos el link a Gmail");
+});
+
+document.getElementById("btnTwitt").href = "https://twitter.com";
+
+document.getElementById("Twitter").addEventListener("click", function () {
+  console.log("Link a Twitter");
+});
+
+document.getElementById("btnWeb").href = "https://time.is/";
+
+document.getElementById("Web").addEventListener("click", function () {
+  console.log("Link a Webpage");
+});
